01635261f0e909b20cb6007a409e688eb077cda3
woohm402 <woohm404@gmail.com>
2023-02-07 00:12:56 +0900

    truffle/browser 기본 기능 구현

--- packages/truffle-browser/src/index.ts
+++ packages/truffle-browser/src/index.ts
@@ -1,7 +1,69 @@
-export const add = (a: number, b: number) => {
-  return a + b;
-};
+export interface TruffleClient {
+  capture(message: Error): void;
+}
 
-export const subtract = (a: number, b: number) => {
-  return a - b;
+export interface TruffleConfig {
+  enabled?: boolean;
+  app: { name: string; phase: string };
+  apiKey: string;
+}
+
+const baseUrl = "https://truffle-api.wafflestudio.com";
+const runtime = { name: "browser", version: "" };
+
+export const getTruffleClient = ({
+  enabled = true,
+  app,
+  apiKey,
+}: TruffleConfig): TruffleClient => {
+  const send = (body: unknown) =>
+    fetch(`${baseUrl}/events`, {
+      method: "POST",
+      headers: { "x-api-key": apiKey, "content-type": "application/json" },
+      body: JSON.stringify(body),
+    });
+
+  return {
+    capture: (error: Error) => {
+      try {
+        if (!enabled) return;
+
+        const message = error.message;
+        const description = window.location.href;
+        const elements = [
+          {
+            className: error.stack ?? "",
+            methodName: "",
+            lineNumber: 0,
+            fileName: "",
+            isInAppInClude: true,
+          },
+        ];
+
+        const body = {
+          version: "v1",
+          app,
+          runtime,
+          description,
+          exception: { className: error.name, message, elements },
+        };
+
+        send(body);
+      } catch (err) {
+        send(
+          JSON.stringify({
+            version: "v1",
+            app,
+            runtime,
+            description: "",
+            exception: {
+              className: "sdk error",
+              message: "sdk error",
+              elements: [],
+            },
+          })
+        );
+      }
+    },
+  };
 };
