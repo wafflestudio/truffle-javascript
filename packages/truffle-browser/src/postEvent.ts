9ae409c6c693456aeffdd795e02ecabbae003cf0
woohm402 <woohm404@gmail.com>
2025-03-31 17:36:36 +0900

    eslint & prettier -> biome

--- packages/truffle-browser/src/postEvent.ts
+++ packages/truffle-browser/src/postEvent.ts
@@ -1,8 +1,8 @@
-const baseUrl = "https://truffle-api.wafflestudio.com";
+const baseUrl = 'https://truffle-api.wafflestudio.com';
 
 export const postEvent = (apiKey: string, body: unknown) =>
   fetch(`${baseUrl}/events`, {
-    method: "POST",
-    headers: { "x-api-key": apiKey, "content-type": "application/json" },
+    method: 'POST',
+    headers: { 'x-api-key': apiKey, 'content-type': 'application/json' },
     body: JSON.stringify(body),
   });
